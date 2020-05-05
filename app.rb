0d8245a627141dc2264052efbcb4e694c0692e24
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-05 19:02:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,9 +21,8 @@ class App < Sinatra::Base
   
   get '/multiply/:num1/:num2' do
     @product = params[:num1].to_i*params[:num2].to_i
-    @num1 = params[:num1]
-    @num2 = params[:num2]
-    "#{@num1*@num2}"
+
+    "#{@product}"
   end 
   
   
